c083fffb7f185a6e2a6fe07e9ca70ecf99284f91
Jonathan <jonathandavis@gilsons.org>
2014-02-21 18:13:04 -0500

    more informative git update script

--- .update.sh
+++ .update.sh
@@ -1,8 +1,11 @@
 cd ~/
+echo "home folder"
 git pull
 cd ~/.oh-my-zsh/
+echo "oh my zsh"
 git commit -m 'blah blah blah'
 git pull --rebase --stat origin master
-cd ~/.oh-my-zsh/custom
+cd ~/.oh-my-zsh/custom/
+echo "zsh custom"
 git pull
 cd
