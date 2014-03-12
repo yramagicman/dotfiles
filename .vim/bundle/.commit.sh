f0720df2f8b70c39b911fdf06340ed4ee1d453d6
Jonathan <jonathandavis@gilsons.org>
2014-03-12 18:53:02 -0400

    blah blah blah

--- .vim/bundle/.commit.sh
+++ .vim/bundle/.commit.sh
@@ -1,3 +1,2 @@
+find . -type f -name '*.DS_Store' -maxdepth 2 -type d -exec git rm .DS_Store \;
 find . -maxdepth 1 -type d -exec git commit --all -q -m 'blah blah blah' \;
-find . '.DS_Store' -maxdepth 2 -type d -exec git rm .DS_Store \;
-
