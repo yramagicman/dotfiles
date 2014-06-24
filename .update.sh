40d5064acac039c951ca9ad23109fa6febe73060
Jonathan <jonathandavis@gilsons.org>
2014-06-23 20:55:20 -0400

    grab mutt changes as well

--- .update.sh
+++ .update.sh
@@ -1,11 +1,14 @@
-cd ~/
+builtin cd ~/
 echo "home folder"
 git pull
-cd ~/.oh-my-zsh/
+builtin cd ~/.oh-my-zsh/
 echo "oh my zsh"
 git commit -m 'blah blah blah'
 git pull --rebase --stat origin master
-cd ~/.oh-my-zsh/custom/plugins/zsh-aliases/
+builtin cd ~/.oh-my-zsh/custom/plugins/zsh-aliases/
 echo "zsh custom"
+git pull
+builtin cd ~/.mutt
+git commit -m 'mail stuff'
 git pull
 cd
