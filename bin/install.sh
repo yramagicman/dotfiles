decf8d2d5aca779b7e2bbfb7796cb2c37b2580b1
Jonathan <jonathandavis@gilsons.org>
2014-12-19 21:32:34 -0500

    typo

--- bin/install.sh
+++ bin/install.sh
@@ -3,5 +3,5 @@ cp -rv ./* ~/
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 cd ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/yramagicman/zsh-aliases
-mdkir -p ~/.oh-my-zsh/custom/themes
+mkdir -p ~/.oh-my-zsh/custom/themes
 ln -s ~/.oh-my-zsh/custom/plugins/zsh-aliases/theme.zsh-theme ~/.oh-my-zsh/custom/themes/theme.zsh-theme
