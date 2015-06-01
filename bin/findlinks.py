046ac207ce9e7287bb5a413f15b8c4c41ef16252
Jonathan <jonathandavis@gilsons.org>
2015-05-31 23:08:23 -0400

    read from stdin

--- bin/findlinks.py
+++ bin/findlinks.py
@@ -1,15 +1,15 @@
-f = open('./Lecture-notes-and-assignments.html')
+import fileinput
+#f = open('./Lecture-notes-and-assignments.html')
 links = []
-for line in f:
+for line in fileinput.input():
   if '<a href' in line:
     link = line.split('<a href')
     for tag in link:
       if '</a>' in tag:
         link = tag[:tag.index('</a>')]
         link = link[:link.rindex('">')]
-        link = link[link.index('=') + 1 :]
+        link = link[link.index('=') + 2 :]
         links.append(link)
 
-f.close()
 for l in links:
   print l
