c407470b741e97b7e0e918e892f5a3cd565f5399
Jonathan <jonathandavis@gilsons.org>
2015-06-08 01:55:33 -0400

    files

---
+++ bin/gup.rkt
@@ -0,0 +1,85 @@
+#lang racket
+(require racket/system)
+(require racket/trace)
+
+(define (execute-command proc-name)
+    (define proc (find-executable-path proc-name))
+    (lambda (args)
+	(with-output-to-string
+	    (thunk (apply system* proc args)))))
+
+(define home (path->string (find-system-path 'home-dir)))
+(define repos (list home ".mutt" ".zprezto" ".zprezto/modules/zsh-aliases" ".password-store"))
+(define git [find-executable-path "git"])
+
+(define (makepath p)
+      (if (not (equal? home p))
+	   (string->path (string-append home p))
+	   (string->path home)))
+
+(define paths (map makepath repos))
+
+(define (changes)
+    (define ch (string-split ((execute-command "git") (list "status")) "\n" #:trim? #t))
+    (define not-empty (filter
+	(lambda (x)
+	    (and (not (string=? "" x)) (> (string-length x) (string-length "modified")))) ch))
+    (define return-value (filter
+	(lambda (y)
+	    (string=? "modified" (substring y 1 (string-length "\tmodified")))) not-empty))
+    (map (lambda (z)
+	(substring z 1)) return-value))
+
+(define num-changes (length (changes)))
+
+(define (show-changes)
+  (display-lines (changes)))
+
+(define (check-status)
+    (define commands
+	'(("checkout" "-q" "master")
+	  ("rev-parse" "@")
+	  ("rev-parse" "@{u}")
+	  ("merge-base" "@" "@{u}")))
+    (define result (map (execute-command "git") commands))
+    (let ([local (second result)]
+	  [remote (third result)]
+	  [base (fourth result)])
+	(cond
+	    ((string=? local remote) 0)
+	    ((string=? base remote) 1)
+	    ((string=? base local) -1)
+	    (else 2))))
+
+(define (pull-push pull push)
+    ((execute-command "git") "fetch")
+    (define stat (check-status))
+    (cond
+	((not (= stat 0))
+	(cond
+	    ((= stat -1) (pull))
+	    ((= stat 1) (push))
+	    (else ((pull) (push)))))
+	    (else ("HI"))))
+
+(define (pull)
+    (let ([pass (list "pass" "git" "pull")]
+	  [git (list "git" "up")]
+	  [zprez (list (string->path (string-append home "bin/zupdate")))]
+	  [pwd (string->path (current-directory))])
+      (cond
+	  ((string=? (string-append home "/.zprezto") pwd) zprez)
+	  ((string=? (string-append home "/.password-store") pwd) pass)
+	  (else git))))
+
+(define (push)
+    (let ([pass (list "pass" "git" "push")]
+	  [git (list "git" "push")]
+	  [pwd (string->path (current-directory))])
+      (cond
+	  ((string=? (string-append home "/.password-store") pwd) pass)
+	  (else git))))
+
+(print (check-status))
+(show-changes)
+(print num-changes)
