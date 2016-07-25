fd6baeb1519287fd780923b4d82c5e3117907482
Jonathan <jonathandavis@gilsons.org>
2016-07-25 15:24:30 -0400

    Don't need that running anymore

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -8,7 +8,7 @@ check_process(){
        $1 &
     fi
 }
-(check_process /home/jonathan/bin/dwmstatus)
+#(check_process /home/jonathan/bin/dwmstatus)
 xflux -z 45036
 ( /home/jonathan/.screenlayout/default.sh) &
 ## Set root window colour
