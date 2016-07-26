b50c8f9671ff7053729e337458ee5b1e82f7d52b
Jonathan <jonathandavis@gilsons.org>
2016-07-26 14:48:58 -0400

    better manage.py

--- bin/manage.py
+++ bin/manage.py
@@ -4,34 +4,14 @@ import sys
 import subprocess
 
 
-def find_managepy():
-    env = os.getenv('VIRTUAL_ENV').split('/')[-1]
-    for file in os.walk(os.getcwd()):
-        if '.git' not in file[0]:
-            if 'manage.py' in file[-1] and env in file[0]:
-                os.chdir(file[0])
-                command = [os.getcwd() + '/manage.py'] + sys.argv[1:]
-                print(' '.join(command))
-                subprocess.call(command)
-                return 1
-
-
-def go_back(found):
-    if not found:
-        path = os.getcwd().split('/')[:-1]
-    if '/'.join(path) == os.path.expanduser('~'):
-        print('No manage.py file found for this project')
-        return
-    return '/'.join(path)
-
-
-def manage():
-    try:
-        found = find_managepy()
-        if not found:
-            back = go_back(found)
-            os.chdir(back)
-            manage()
-    except KeyboardInterrupt:
-        exit()
-manage()
+env = os.getenv('VIRTUAL_ENV')
+if '.virtualenvs' in env:
+    with open(env + '/.project') as f:
+        proj_root = f.readline()
+        print(proj_root.strip() + '/manage.py')
+        subprocess.call([proj_root.strip() + '/manage.py'] + sys.argv[1:])
+        f.close()
+else:
+    proj_root = env
+    print(proj_root.strip() + '/manage.py')
+    subprocess.call([proj_root.strip() + '/manage.py'] + sys.argv[1:])
