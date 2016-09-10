bdf3cd1cc958e282973a30b93ccf409d316a475a
Jonathan <jonathandavis@gilsons.org>
2016-09-10 18:42:04 -0400

    better notifications

--- bin/mailnotify.py
+++ bin/mailnotify.py
@@ -8,8 +8,15 @@ pynotify.init("Basic")
 while True:
     mails = subprocess.check_output(['/home/jonathan/bin/mailmon'])
     if int(mails) > 0:
-        n = pynotify.Notification("You have mail!", mails + "unread messages")
-        n.show()
-        subprocess.call(['mplayer', '/home/jonathan/.sounds/beep.mp3',
-                         '-really-quiet'])
-    time.sleep(60*10)
+        notified = 0
+        while notified <= 5:
+            n = pynotify.Notification("You have mail!", mails +
+                                      "unread messages")
+            n.show()
+            subprocess.call(['mplayer', '/home/jonathan/.sounds/beep.mp3',
+                             '-really-quiet'])
+            time.sleep(60*5)
+            notified = notified + 1
+            print notified
+        time.sleep(60*10)
+    time.sleep(10)
