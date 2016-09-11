98d6bb92e1d3e568411f1591186603c141df1129
Jonathan <jonathandavis@gilsons.org>
2016-09-11 09:36:42 -0400

    don't notify if mail has been checked

--- bin/mailnotify.py
+++ bin/mailnotify.py
@@ -9,7 +9,7 @@ while True:
     mails = subprocess.check_output(['/home/jonathan/bin/mailmon'])
     if int(mails) > 0:
         notified = 0
-        while notified <= 5:
+        while notified <= 5 and mails > 0:
             n = pynotify.Notification("You have mail!", mails +
                                       "unread messages")
             n.show()
@@ -17,6 +17,7 @@ while True:
                              '-really-quiet'])
             time.sleep(60*5)
             notified = notified + 1
+            mails = subprocess.check_output(['/home/jonathan/bin/mailmon'])
             print notified
         time.sleep(60*10)
     time.sleep(10)
