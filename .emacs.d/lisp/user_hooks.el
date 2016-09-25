79e397ab9a913c95ae00f291e4b82fe79b5674b0
Jonathan <jonathandavis@gilsons.org>
2016-09-25 10:16:23 -0400

    hooks for email

--- .emacs.d/lisp/user_hooks.el
+++ .emacs.d/lisp/user_hooks.el
@@ -1,12 +1,10 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'before-save-hook 'evil-normal-state)
+(add-hook 'before-save-hook 'untabify)
 ; make sure files are unix!
 (add-hook 'before-save-hook
           (lambda nil
             (set-buffer-file-coding-system 'undecided-unix)))
-
-
-
 
 (add-hook 'emacs-lisp-mode-hook
       (lambda nil
@@ -16,7 +14,7 @@
 (add-hook 'org-mode-hook 'org-indent-mode)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'after-init-hook 'flyspell-mode)
+(add-hook 'message-mode-hook 'flyspell-mode)
 
 (add-hook 'dired-load-hook
           (lambda ()
@@ -26,4 +24,16 @@
             (setq dired-omit-files "\*pyc")
             (setq dired-omit-files-p t)
             (dired-omit-mode)))
+
+(add-to-list 'auto-mode-alist '("/\\..*zsh\\'" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("*.php" . web-mode))
+(add-to-list 'auto-mode-alist '(".zshrc" . shell-script-mode))
+(add-to-list 'auto-mode-alist '(".zpreztorc" . shell-script-mode))
+(add-to-list 'auto-mode-alist '(".zprofile" . shell-script-mode))
+(add-to-list 'auto-mode-alist '(".muttrc" . muttrc-mode))
+(add-to-list 'auto-mode-alist '("mutt-tardis*" . message-mode))
+(add-to-list 'auto-mode-alist '("mutt-k-nine*" . message-mode))
+
+
+
 (provide 'user_hooks)
