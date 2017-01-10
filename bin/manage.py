8a51d0959aabb6bd732d82a2665e34fcca84709d
Jonathan <jonathandavis@gilsons.org>
2017-01-10 14:35:25 -0500

    remove error message on ^C for manage.py

--- bin/manage.py
+++ bin/manage.py
@@ -3,15 +3,17 @@ import os
 import sys
 import subprocess
 
-
-env = os.getenv('VIRTUAL_ENV')
-if '.virtualenvs' in env:
-    with open(env + '/.project') as f:
-        proj_root = f.readline()
+try:
+    env = os.getenv('VIRTUAL_ENV')
+    if '.virtualenvs' in env:
+        with open(env + '/.project') as f:
+            proj_root = f.readline()
+            print(proj_root.strip() + '/manage.py')
+            subprocess.call([proj_root.strip() + '/manage.py'] + sys.argv[1:])
+            f.close()
+    else:
+        proj_root = env
         print(proj_root.strip() + '/manage.py')
         subprocess.call([proj_root.strip() + '/manage.py'] + sys.argv[1:])
-        f.close()
-else:
-    proj_root = env
-    print(proj_root.strip() + '/manage.py')
-    subprocess.call([proj_root.strip() + '/manage.py'] + sys.argv[1:])
+except KeyboardInterrupt:
+    exit()
