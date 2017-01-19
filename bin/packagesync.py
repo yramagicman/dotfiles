55aa7fae470bcc7036d95b02a24312bc462ebb88
Jonathan <jonathandavis@gilsons.org>
2017-01-19 15:08:33 -0500

    packagesync script

---
+++ bin/packagesync.py
@@ -0,0 +1,35 @@
+import subprocess
+
+hostname = subprocess.run(['hostname'], check=True,
+                          stdout=subprocess.PIPE).stdout
+# print(hostname.decode())
+localpackages = open(hostname.decode().strip())
+
+hostname = hostname.strip()
+if hostname.decode() == 'tardis':
+    print('knine')
+    remotepackages = open('knine')
+else:
+    print('tardis')
+    remotepackages = open('tardis')
+
+remote = []
+for p in remotepackages.readlines():
+   remote.append(p.strip())
+
+for p in localpackages.readlines():
+   package = p[:p.index(' ')]
+   print(p.strip())
+   if p.strip() not in remote:
+       subprocess.run(['sudo', 'pacman', '-R',  package])
+
+
+local = []
+for p in localpackages.readlines():
+    local.append(p.strip())
+
+
+for p in remote:
+    package = p[:p.index(' ')]
+    if p.strip() not in local:
+        subprocess.run(['pacaur', '-S', '--needed', package])
