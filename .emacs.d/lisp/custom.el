82df1abf2a22f7c5c9832e09a06ea1b9a628d022
Jonathan <jonathandavis@gilsons.org>
2017-01-31 17:28:27 -0500

    fonts, also better emacs stuff

--- .emacs.d/lisp/custom.el
+++ .emacs.d/lisp/custom.el
@@ -33,4 +33,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 118 :width normal :family "Input")))))
+ '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "FBI " :family "Inconsolata")))))
