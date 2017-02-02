71b85ee7f96ef3ab20925ae996e46f13668bca0d
Jonathan <jonathandavis@gilsons.org>
2017-02-02 16:12:56 -0500

    findlinks stuff

--- bin/findlinks.py
+++ bin/findlinks.py
@@ -1,14 +1,14 @@
 import fileinput
-#f = open('./Lecture-notes-and-assignments.html')
+# f = open('./Lecture-notes-and-assignments.html')
 links = []
 for line in fileinput.input():
-  if '<a href' in line:
-    link = line.split(' ')
-    for tag in link:
-        t = tag.split('"')
-        if 'href=' in t :
-            print t[1]
+    if '<a href' in line:
+        link = line.split(' ')
+        for tag in link:
+            t = tag.split('"')
+            if 'href=' in t:
+                print t[1]
 
 
-#for l in links:
+# for l in links:
 #  print l
